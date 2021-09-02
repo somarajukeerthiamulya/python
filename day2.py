06de41d678eeac549d5ed1ab7af1abc659890611
somarajukeerthiamulya <67212447+somarajukeerthiamulya@users.noreply.github.com>
2021-09-02 15:31:37 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,79 @@
+#prime with while
+"""
+n=int(input())
+c=1
+i=1
+while(n>=i):
+    if(n%i==0):
+        c=c+1
+    i=i+1
+if(c==2):
+    print("prime")
+        
+else:
+    print("not prime")
+"""
+#elif
+"""
+marks = int(input())
+if(marks==100 and marks>=92):
+    print("grade a+")
+elif(marks<=91 and marks>=82):
+    print("grade a")
+elif(marks<=81 and marks>=72):
+    print("grade b+")
+elif(marks<=71 and marks>=62):
+    print("grade b")
+elif(marks<=61 and marks>=52):
+    print("grade c+")
+elif(marks<=51 and marks>=42):
+    print("grade c")
+elif(marks<=41 and marks>=32):
+    print("grade d")
+else:
+    print("fail")
+"""
+#for with else
+"""
+n=int(input())
+for i in range(0,n+1):
+    break;
+else:
+    print(i)
+"""
+#armstrong number
+"""n=int(input())
+for
+"""
+#continue
+"""
+n=int(input())
+for i in range(1,n+1):
+    if(i==3):
+        continue
+    
+    print(i)
+"""
+#functions
+"""
+def a():#function gedinition
+    print("hii")
+    print("helo")
+a()#function call
+"""
+#add function
+"""
+def add(n,m):
+    print(n+m)
+n=int(input())
+m=int(input())
+add(n,m)
+"""
+
+
+    
+    
+    
+          
+        
+            
